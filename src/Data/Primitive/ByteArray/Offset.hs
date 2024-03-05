2221f2f5993f481c8fe3e87de90b78aa0e6e210c
Brian McKeon <135748266+brianjosephmckeon@users.noreply.github.com>
2024-03-05 10:54:34 -0500

    Prepare 0.2.0.1 release.
    Reformatted.
    Use new .github workflows.
    Updated package metadata.

--- src/Data/Primitive/ByteArray/Offset.hs
+++ src/Data/Primitive/ByteArray/Offset.hs
@@ -1,21 +1,24 @@
-{-# language DuplicateRecordFields #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Data.Primitive.ByteArray.Offset
   ( -- * Types
-    ByteArrayOffset(..)
-  , MutableByteArrayOffset(..)
+    ByteArrayOffset (..)
+  , MutableByteArrayOffset (..)
   ) where
 
-import Data.Primitive (ByteArray,MutableByteArray)
+import Data.Primitive (ByteArray, MutableByteArray)
 
--- | A byte array and an index into the array. The element
--- type is understood to be byte (an 8-bit word).
+{- | A byte array and an index into the array. The element
+type is understood to be byte (an 8-bit word).
+-}
 data ByteArrayOffset = ByteArrayOffset
   { array :: {-# UNPACK #-} !ByteArray
   , offset :: {-# UNPACK #-} !Int
   }
 
--- | A mutable byte array and an index into the array. The element
--- type is understood to be byte (an 8-bit word).
+{- | A mutable byte array and an index into the array. The element
+type is understood to be byte (an 8-bit word).
+-}
 data MutableByteArrayOffset s = MutableByteArrayOffset
   { array :: {-# UNPACK #-} !(MutableByteArray s)
   , offset :: {-# UNPACK #-} !Int
